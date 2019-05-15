5c44ffa19a1bf7f6c61e8e0e02a09da6790af888
viky yahya <37471218+vikyyahya@users.noreply.github.com>
2019-05-15 13:53:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dataa Mahasiswa</title>
+    <title>Data Mahasiswa</title>
     
 </head>
 <body>
 
 <?php
 echo "hallo word";
-try {
-    $conn = mysqli_connect ("https://submis1datamahasiswa.azurewebsites.net","viky","aaaaaatakcodingwebappserverA1","dbMahasiswa") or die(mysqli_error());
+
+    $conn = mysqli_connect ("takcodingwebappserver.database.windows.net","viky","aaaaaatakcodingwebappserverA1","dbMahasiswa") or die(mysqli_error());
     
-}
-catch (Exception $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
     
 if ($conn)
 {
